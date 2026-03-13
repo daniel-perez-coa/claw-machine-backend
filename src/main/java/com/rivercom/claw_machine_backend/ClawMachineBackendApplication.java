08cefb3411c2ba1a3641da1b136d9d825fc1784b
Daniel David Perez Coapango <dano.prez.coa@gmail.com>
2026-03-12 19:41:23 -0600

    Initial commit

---
+++ src/main/java/com/rivercom/claw_machine_backend/ClawMachineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rivercom.claw_machine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClawMachineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClawMachineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rivercom/claw_machine_backend/ClawMachineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rivercom.claw_machine_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClawMachineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
